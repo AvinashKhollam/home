27611116fce8088b581f9de5592cb12fd0d72fe1
avinash.khollam <avinash.khollam83@gmail.com>
2024-01-02 18:46:23 +0530

    commiting demoController file

--- src/main/java/com/home/demo/controllers/DemoController.java
+++ src/main/java/com/home/demo/controllers/DemoController.java
@@ -1,12 +1,16 @@
 package com.home.demo.controllers;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DemoController{
 
-    @GetMapping
-    public String getName(String str){
-        return str;
+    @GetMapping("name")
+    public String getName(@RequestParam String str){
+        return "hi "+str;
     }
 }
